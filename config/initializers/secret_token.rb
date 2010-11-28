61d6f65272fa674d45da25f7eec5b4cd552f9e73
Simon Chiu <skhchiu@gmail.com>
2010-11-28 09:36:02 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CallerApp::Application.config.secret_token = '3657c1382387ed813f04bd796fcf227b4dbd861b55db5175d451b9381304730ddc962ae380cf1a1068f1bc90b4964712bfb60df6afe878b66ae57751607d37bc'
